95be365e6583e73f7e51c9593710e9585bffd96b
wuleying <5590548@qq.com>
2018-04-24 14:02:23 +0800

    Update

--- block.go
+++ block.go
@@ -1,25 +1,37 @@
 package main
 
-import "time"
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
 
 // Block 区块结构体
 type Block struct {
-	Id            int64
-	Timestamp     int64
-	PrevBlockHash []byte
-	Hash          []byte
-	Nonce         int
+	Timestamp     int64  // 当前时间戳，也就是区块创建的时间
+	Data          []byte // 区块存储的实际有效的信息
+	PrevBlockHash []byte // 存储的是前一个块的哈希
+	Hash          []byte // 当前块的哈希
 }
 
-// NewBlock 创建新区块
-func NewBlock(prevId int64, prevBlockHash []byte) *Block {
-	id := prevId + 1
-	block := &Block{id, time.Now().Unix(), prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
+// SetHash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
 
-	block.Hash = hash[:]
-	block.Nonce = nonce
+	b.Hash = hash[:]
+}
 
+// NewBlock
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
 	return block
+}
+
+// NewGenesisBlock 创世区块
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
 }
--- global.go
+++ global.go
@@ -6,18 +6,8 @@ import "math"
 const (
 	VERSION = byte(0x00)
 
-	// Last hash key
-	LAST_HASH_KEY = "lastHash"
-	// Bucket名称
-	BLOCK_BUCKET_NAME = "blocks"
-
-	// 创世币数据
-	GENESIS_COIN_BASEDATA = "hello luoliang"
-
 	// 挖矿难度
 	MINE_TARGET_BITS = 18
-	// 挖矿奖励
-	MINE_SUBSIDY = 10
 	// Number once最大值
 	MAX_NONCE = math.MaxInt64
 
